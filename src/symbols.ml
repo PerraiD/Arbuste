627a67fa277efaf3e6fd3c48f196e1734e8c2dff
Benoît Guédas <benoit@guedas.com>
2014-03-02 14:51:49 +0100

    Symbols.add replaces the prvious definition with the new one

--- src/symbols.ml
+++ src/symbols.ml
@@ -4,11 +4,14 @@ type t = (string * Ast.t) list
 
 let create () = []
 
+(** Tells whether [key] belongs to [env]. *)
 let mem env key = List.mem_assoc key env 
 
 let add env key value =
   if mem env key
-    then env
+    then
+      let l = List.remove_assoc key env in
+      (key,value) :: l
     else (key, value) :: env
 
 let find env key = List.assoc key env
