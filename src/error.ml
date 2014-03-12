79c93020483e8a7ff64485bf923060e9d0512f80
Benoît Guédas <benoit@guedas.com>
2014-03-12 20:17:56 +0100

    Fix positioned error printing

--- src/error.ml
+++ src/error.ml
@@ -9,10 +9,7 @@ exception ArbusteError of arbuste_exception
 let raise_simple message = raise (ArbusteError (Simple message))
 
 let raise_positioned s p =
-  let l_pos = string_of_int p.pos_lnum in
-  let c_pos = string_of_int (p.pos_bol + 1) in
-  let message = "Error line " ^ l_pos ^ " character " ^ c_pos ^ ": " ^ s in
-  raise (ArbusteError (Positioned (message, p)))
+  raise (ArbusteError (Positioned (s, p)))
 
 let print = function
   | Simple m -> Printf.eprintf "Error: %s\n" m
