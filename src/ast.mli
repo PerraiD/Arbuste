d36d40345a3f37aaa996aaa482c05e62aac14c71
Benoît Guédas <benoit@guedas.com>
2014-03-27 08:11:21 +0100

    Add an mli file to ast

---
+++ src/ast.mli
@@ -0,0 +1,55 @@
+module Operator :
+  sig
+    type t =
+        Add
+      | Sub
+      | Mul
+      | Div
+      | Or
+      | And
+      | Lesser
+      | Greater
+      | Equal
+      | Print
+      | Read
+      | Seq
+      | Let
+      | In
+      | If
+      | Branch
+      | Func
+      | Param
+      | Eval
+      | Cast
+    val to_string : t -> string
+    val print : t -> unit
+    val print_endline : t -> unit
+  end
+
+module Operand :
+  sig
+    type t =
+        Void
+      | EndParam
+      | Stdout
+      | Stdin
+      | ToString
+      | ToInt
+      | ToBool
+      | String of string
+      | Int of int
+      | Bool of bool
+      | Ident of string
+    val to_string : t -> string
+    val print : t -> unit
+    val print_endline : t -> unit
+  end
+
+type t = {contents :ast; position : Lexing.position}
+and ast = Operand of Operand.t | Operation of Operator.t * t * t
+
+val to_string : t -> string
+
+val print : t -> unit
+
+val print_list : t list -> unit
