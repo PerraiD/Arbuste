ada377d1c18e8ef3fcb8d759ba3aa945f0e1a6d8
Benoît Guédas <benoit@guedas.com>
2014-03-27 06:32:15 +0100

    Symflify the arbuste exception type

--- src/error.ml
+++ src/error.ml
@@ -1,18 +1,13 @@
 open Lexing
 
-type arbuste_exception =
-  | Simple of string
-  | Positioned of string * Lexing.position
+type arbuste_exception = string * Lexing.position
 
 exception ArbusteError of arbuste_exception
 
-let raise_positioned s p =
-  raise (ArbusteError (Positioned (s, p)))
+let raise_positioned m p = raise (ArbusteError (m, p))
 
-let print = function
-  | Simple m -> Printf.eprintf "Error: %s\n" m
-  | Positioned (m, p) -> Printf.eprintf "Error line %d character %d: %s\n"
-                         p.pos_lnum (p.pos_bol + 1) m
+let print (m,p) =
+  Printf.eprintf "Error line %d character %d: %s\n" p.pos_lnum (p.pos_bol + 1) m
 
 let warn_shadowed id p =
    Printf.eprintf "Warning line %d character %d: previous definition of %s is shadowed\n"
