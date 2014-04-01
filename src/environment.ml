e4d4c05c7c389751d612b1906c88d7a82d0140d0
Benoît Guédas <benoit@guedas.com>
2014-04-01 06:24:34 +0200

    Remove ocamldoc from the ml file, and add named parameters.

--- src/environment.ml
+++ src/environment.ml
@@ -5,45 +5,36 @@ type symbol = Ast.t * (Ast.t list)
 
 type t = (string * symbol) list
 
-(** Creates a new environment. *)
 let create () = []
 
-(** Prints an [env]ironment. *)
 let print env =
   let f = function
     | (k, (v, [])) -> Printf.printf "var: %s = %s\n" k (Ast.to_string v)
     | (k, (v, p)) -> Printf.printf "func: %s()\n" k in
   List.iter f env
 
-(** Creates an environment with the given identifiers associated to the given
-    values. *)
-let make_env idents values =
+let make_env ~keys ~values =
   let f x y = match x.data with
     | Operand (Ident i) -> (i, (y, []))
     | _ -> Error.error "Bad arguments" x.position
   in
-  List.map2 f idents values
+  List.map2 f keys values
  
-(** Tells whether [key] belongs to [env]. *)
-let mem env key = List.mem_assoc key env 
+let mem ~env ~key = List.mem_assoc key env 
 
-(** Adds a functionction definition with its parameters to an environment. *)
-let add_fun env key value params =
+let add_fun ~env ~key ~value ~params =
   if mem env key
     then
       let l = List.remove_assoc key env in
       (key,(value, params)) :: l
     else (key, (value, params)) :: env
 
-(** Adds a variable to the environment. *)
-let add env key value = add_fun env key value []
+let add ~env ~key ~value = add_fun env key value []
 
-(** Finds a variable value in the environment. *)
-let find env key =
+let find ~env ~key =
   try Some (fst (List.assoc key env))
   with Not_found -> None
 
-(** Finds a function definition in the environment. *)
-let find_func env key =
+let find_func ~env ~key =
   try Some (List.assoc key env)
   with Not_found -> None
--- src/environment.mli
+++ src/environment.mli
@@ -3,24 +3,24 @@ type t
 (** Creates a new environment. *)
 val create : unit -> t
 
-(** Prints an [env]ironment. *)
+(** Prints an environment. *)
 val print : t -> unit
 
 (** Creates an environment with the given identifiers associated to the given
     values. *)
-val make_env : Ast.t list -> Ast.t list -> t
+val make_env : keys:Ast.t list -> values:Ast.t list -> t
  
-(** Tells whether [key] belongs to [env]. *)
-val mem : t -> string -> bool 
+(** Tells whether key belongs to environment. *)
+val mem : env:t -> key:string -> bool 
 
 (** Adds a functionction definition with its parameters to an environment. *)
-val add_fun : t -> string -> Ast.t -> Ast.t list -> t
+val add_fun : env:t -> key:string -> value:Ast.t -> params:Ast.t list -> t
 
 (** Adds a variable to the environment. *)
-val add : t -> string -> Ast.t -> t
+val add : env:t -> key:string -> value:Ast.t -> t
 
 (** Finds a variable value in the environment. *)
-val find : t -> string -> Ast.t option
+val find : env:t -> key:string -> Ast.t option
 
 (** Finds a function definition in the environment. *)
-val find_func : t -> string -> (Ast.t * Ast.t list) option
+val find_func : env:t -> key:string -> (Ast.t * Ast.t list) option
